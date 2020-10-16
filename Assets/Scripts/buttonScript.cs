d6ba98d679ed439010e5bcf0b19c891b96f7eb49
Ablang-E <ablange.eric@gmail.com>
2020-10-16 16:41:30 -0500

    Final Version 1.1

---
+++ Assets/Scripts/buttonScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Vuforia;
+
+public class buttonScript : MonoBehaviour
+{
+
+    public GameObject buttonObj;
+    public Animator animation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //animation.GetComponent<Animator>();
+        //buttonObj = GameObject.Find("changeButton");
+        //buttonObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        buttonObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
+        buttonObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour  vb) {
+        animation.SetBool("buttonPressed", true);
+        Debug.Log("Button was pressed!");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb) {
+        animation.SetBool("buttonPressed", false);
+        Debug.Log("Button was released!");
+    }
+}
---
+++ customSoundScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class customSoundScript : MonoBehaviour
+{
+    public AudioClip customSound;
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void playSound() {
+        audioSource.PlayOneShot(customSound);
+    }
+}
